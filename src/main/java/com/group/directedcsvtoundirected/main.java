02a8a4158adbed4687dc6f4682ac9de37058f950
Gevoooo <91430969+Gevoooo@users.noreply.github.com>
2024-02-04 17:37:54 -0500

    Feat: Open file chooser dialog and save selected csv file

--- src/main/java/com/group/directedcsvtoundirected/main.java
+++ src/main/java/com/group/directedcsvtoundirected/main.java
@@ -1,11 +1,26 @@
 package com.group.directedcsvtoundirected;
 
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  *
  * @author Gevoooo
  */
 public class main {
     public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("./"));
+        File inputFile = null;
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            inputFile = fileChooser.getSelectedFile();
+        }
         
+        try {
+            FileReader fileReader = new FileReader(inputFile);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
