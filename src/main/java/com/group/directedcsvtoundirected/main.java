be8de4bb27a5b890a80ab4677e606e727af77061
Gevoooo <91430969+Gevoooo@users.noreply.github.com>
2024-02-04 18:03:08 -0500

    Feat: Write all cells and flipped cells into new csv file

--- src/main/java/com/group/directedcsvtoundirected/main.java
+++ src/main/java/com/group/directedcsvtoundirected/main.java
@@ -1,9 +1,11 @@
 package com.group.directedcsvtoundirected;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import javax.swing.JFileChooser;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 /**
  *
@@ -14,19 +16,25 @@ public class main {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File("./"));
         File inputFile = null;
+        String fileName = "";
         if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             inputFile = fileChooser.getSelectedFile();
+            fileName = fileChooser.getName(fileChooser.getSelectedFile());
         }
         try {
             FileReader fileReader = new FileReader(inputFile);
             CSVReader csvReader = new CSVReader(fileReader);
             String[] nextRecord;
+            
+            FileWriter outputFile = new FileWriter(new File("./" + fileName + "_Undirected.csv"));
+            CSVWriter csvWriter = new CSVWriter(outputFile);
+            
             while ((nextRecord = csvReader.readNext()) != null) {
-                for (String cell : nextRecord) {
-                    System.out.print(cell + "\t");
-                }
-                System.out.println("");
+                String[] flippedRecord = {nextRecord[1], nextRecord[0]};
+                csvWriter.writeNext(nextRecord);
+                csvWriter.writeNext(flippedRecord);
             }
+            csvWriter.close();
         } catch(Exception e) {
             e.printStackTrace();
         }
